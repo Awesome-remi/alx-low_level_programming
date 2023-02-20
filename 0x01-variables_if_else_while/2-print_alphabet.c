bcd3e733ffd674b89e744f071af36f2ac0995830
Abatan remi <abatansamuel.sa@gmail.com>
2023-02-20 08:58:54 -0800

    Print Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main _ prints the alphabet in lower case,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char ch;
+
+	for (ch = "a"; ch <= 'z'; ch++)
+
+	{
+		putcht(ch);
+	}
+
+	putchar("\n');
+
+	return (0)
+}
