7abbb9cccd6a4094b342f4288e383dc1555ad678
Abatan remi <abatansamuel.sa@gmail.com>
2023-02-21 07:12:21 -0800

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
