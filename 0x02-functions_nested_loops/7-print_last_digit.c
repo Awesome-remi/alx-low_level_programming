814ebb6c6478f176e487d9fa861a17a229bc85de
Abatan remi <abatansamuel.sa@gmail.com>
2023-02-21 11:48:58 -0800

    go

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * prints_last_digit - prints the last digit of a number
- * @n: the int to extract the last digit from
+ * @n:the int to extract the last digit from
  * Return: value of the last digit
  */
 
